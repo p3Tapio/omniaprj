af09722c364b922a11be62e54f4f297523aae660
p3Tapio <55560071+p3Tapio@users.noreply.github.com>
2020-03-30 21:33:55 +0300

    Add files via upload

---
+++ precache-manifest.b37948cba26faff573369fba257934ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccac162a4667d31ef0c368454eb59bed",
+    "url": "http://petrila.azurewebsites.net/index.html"
+  },
+  {
+    "revision": "7a807963f0ec564c9fb2",
+    "url": "http://petrila.azurewebsites.net/static/css/2.a85f887a.chunk.css"
+  },
+  {
+    "revision": "7a807963f0ec564c9fb2",
+    "url": "http://petrila.azurewebsites.net/static/js/2.931f6ee9.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "http://petrila.azurewebsites.net/static/js/2.931f6ee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b0ccf8df225d64394be",
+    "url": "http://petrila.azurewebsites.net/static/js/main.67fa6513.chunk.js"
+  },
+  {
+    "revision": "3cdb821e80e5962d274b",
+    "url": "http://petrila.azurewebsites.net/static/js/runtime-main.4d8f19e6.js"
+  }
+]);
